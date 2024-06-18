2222eaf84978918d2b7835c7bd1e30910d067fc9
HariPrasath1996 <hasathphy96@gmail.com>
2024-06-18 22:24:57 +0530

    delete function completed

--- crud/src/App.jsx
+++ crud/src/App.jsx
@@ -6,41 +6,52 @@ import { useEffect, useState } from "react"
 
 function App() {
   const [users, setUsers] = useState([])
-  const [filterTextValue,setFilterTextValue]=useState([])
+  const [filterTextValue, setFilterTextValue] = useState([])
+  const [isRecordOpen,setIsRecordOpen]=useState(false);
+  // this below state variable create for add or edit to pass to api , 
+  const [addData,setAddData]=useState([{name : "",age : "",city : ""}])
 
   // get data from api to using axios method
-const getAllUsers=async()=>{
-  await axios.get("http://localhost:8000/users").then((res)=>
-    {
+  const getAllUsers = async () => {
+    await axios.get("http://localhost:8000/users").then((res) => {
       // console.log(res.data)
-    setUsers(res.data)
-    setFilterTextValue(res.data)
+      setUsers(res.data)
+      setFilterTextValue(res.data)
+    }
+    )
   }
-  )
-}
   // use Effect for once app is running data will render one time , bcoz of i selected here empty dependency array
   useEffect(() => {
     getAllUsers()
+
   }, [])
 
-// search and get value in this component
-  const handleSearch =(e)=>{
-const searchText=e.target.value.toLowerCase()
-// the value get from users
-const filterText = users.filter((user)=>user.name.toLowerCase().includes(searchText) || user.city.toLowerCase().includes(searchText))
-setFilterTextValue(filterText)
+  // search and get value in this component
+  const handleSearch = (e) => {
+    const searchText = e.target.value.toLowerCase()
+    // the value get from users
+    const filterText = users.filter((user) => user.name.toLowerCase().includes(searchText) || user.city.toLowerCase().includes(searchText))
+    setFilterTextValue(filterText)
   }
 
   // delete method ... 
-const handleDelete = async(id)=>{
-  await axios.delete(`http://localhost:8000/users/${id}`).then((res)=>{
-    setUsers(res.data)
-    setFilterTextValue(res.data)
-  });
-  // after completion this method , create route method to server point to access this delete function
-};
+  const handleDelete = async (id) => {
+    const isConfirmed = window.confirm("Are You sure , You want to delete this user ?")
+    if (isConfirmed) {
+      await axios.delete(`http://localhost:8000/users/${id}`).
+        then((res) => {
+          setUsers(res.data)
+          setFilterTextValue(res.data)
+        });
+      // after completion this method , create route method to server point to access this delete function
+    };
+  }
 
+  // Add or create  method 
+  
+const handleCreateRecord = ()=>{
 
+}
 
   return (
     <>
@@ -63,20 +74,20 @@ const handleDelete = async(id)=>{
             </>
           </thead>
           <tbody>
-{
-  filterTextValue && filterTextValue.map((user,index)=>{
-    return (
-      <tr key={user.id} >
-      <td>{index+1}</td>
-      <td>{user.name}</td>
-      <td>{user.age}</td>
-      <td>{user.city}</td>
-      <td><button className='btn-edit green'>Edit</button></td>
-      <td><button onClick={()=>handleDelete(user.id)} className='btn-delete red'>Delete</button></td>
-    </tr>
-    )
-  })
-}
+            {
+              filterTextValue && filterTextValue.map((user, index) => {
+                return (
+                  <tr key={user.id} >
+                    <td>{index + 1}</td>
+                    <td>{user.name}</td>
+                    <td>{user.age}</td>
+                    <td>{user.city}</td>
+                    <td><button className='btn-edit green'onClick={handleCreateRecord}>Edit</button></td>
+                    <td><button className='btn-delete red' onClick={() => handleDelete(user.id)}>Delete</button></td>
+                  </tr>
+                )
+              })
+            }
           </tbody>
         </table>
       </div>
