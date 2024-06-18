b68f9fd8e7453175c385432d88e6388039245233
HariPrasath1996 <hasathphy96@gmail.com>
2024-06-18 18:33:12 +0530

    search component completed

--- crud/src/App.jsx
+++ crud/src/App.jsx
@@ -1,33 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import axios for api data receive
+import axios from "axios"
 import './App.css'
+import { useEffect, useState } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([])
+  const [filterTextValue,setFilterTextValue]=useState([])
+
+  // get data from api to using axios method
+const getAllUsers=async()=>{
+  await axios.get("http://localhost:8000/users").then((res)=>
+    {
+      // console.log(res.data)
+    setUsers(res.data)
+    setFilterTextValue(res.data)
+  }
+  )
+}
+  // use Effect for once app is running data will render one time , bcoz of i selected here empty dependency array
+  useEffect(() => {
+    getAllUsers()
+  }, [])
+// search and get value in this component
+  const handleSearch =(e)=>{
+const searchText=e.target.value.toLowerCase()
+// the value get from users
+const filterText = users.filter((user)=>user.name.toLowerCase().includes(searchText) || user.city.toLowerCase().includes(searchText))
+setFilterTextValue(filterText)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <h3>Crud Application With React js Front End and Back End Node Js</h3>
+        <div className='input-container'>
+          <label htmlFor="Search" >Title : </label>
+          <input onChange={handleSearch} placeholder="search here..." type="Search" id='Search' className='Search' />
+          <button className='search-btn'>Add Record</button>
+        </div>
+        <table className='table-head'>
+          <thead >
+            <>
+              <th>S.No</th>
+              <th>Name</th>
+              <th>Age</th>
+              <th>City</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </>
+          </thead>
+          <tbody>
+{
+  filterTextValue && filterTextValue.map((user,index)=>{
+    return (
+      <tr key={user.id} >
+      <td>{index+1}</td>
+      <td>{user.name}</td>
+      <td>{user.age}</td>
+      <td>{user.city}</td>
+      <td><button className='btn-edit green'>Edit</button></td>
+      <td><button className='btn-delete red'>Delete</button></td>
+    </tr>
+    )
+  })
+}
+          </tbody>
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require ("express")
 const users = require("./sample.json")
+const cors = require ("cors")
 
 const app = express()
 const port = 8000;
+app.use(cors({
+    origin:"http://localhost:5173",
+    methods:["GET","POST","PATCH","DELETE"]
+}))
 
 // display users 
 app.get("/users",(req,res)=>{
